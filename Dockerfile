f95de8a5f4983017749ed4a2a3afcf1350afa0da
Lautaro Colella <lautarocolella1@gmail.com>
2025-09-19 00:05:48 +0000

    Dockerfile y start.sh comentados

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
+# Imagen de nodejs
 FROM node:18-bullseye
 
+# Se actualiza apt y se instala postgres
 RUN apt-get update && apt-get install -y postgresql postgresql-contrib bash && rm -rf /var/lib/apt/lists/*
 
+# Se instala nestjs (framework de node)
 RUN npm install -g @nestjs/cli
 
+# Nos movemos para no estar en /
 WORKDIR /usr/src/app
 
 COPY package*.json ./
@@ -13,8 +17,12 @@ RUN npm install --production
 COPY . .
 
 COPY start.sh ./start.sh
+
+# Se dan permisos de ejecucion para este binario
 RUN chmod +x ./start.sh
 
+# Se muestra el puerto 5432
 EXPOSE 3000 5432
 
+# Se corre el binario
 CMD ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
+# Inicia postgres
 service postgresql start
 
+# Loop simple para esperar a que postgres se levante para cambiar de usuario
 until pg_isready -h 127.0.0.1 -U postgres; do
   echo "Esperando a PostgreSQL..."
   sleep 2
 done
 
+# Se cambia al usuario postgres y se crea el nuevo usuario docker en la db, con su contrasena. Luego se crea la db
 su - postgres -c "psql -tc \"SELECT 1 FROM pg_user WHERE usename='docker'\" | grep -q 1 || psql -c \"CREATE USER docker WITH SUPERUSER PASSWORD 'docker';\""
 su - postgres -c "psql -tc \"SELECT 1 FROM pg_database WHERE datname='lol_db'\" | grep -q 1 || psql -c \"CREATE DATABASE lol_db OWNER docker;\""
 
+# Parsear y mapear las variables de entorno del archivo .env con las del contenedor
 if [ -f ".env" ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-# npx typeorm migration:run -d src/data-source.ts
-
+# Se buildea nestjs
 npm run build
 
+# Se corre el servidor
 npm run start:prod
